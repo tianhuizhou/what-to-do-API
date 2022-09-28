1a597677879f72690c98f7c82bef2419920e455a
Bobbyzzz <tianhui@ndmglobal.com>
2022-09-28 04:07:19 -1000

    fix: fix error-handler middleware does not return body correctly

--- functions/src/middleware/error_handler.ts
+++ functions/src/middleware/error_handler.ts
@@ -4,7 +4,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 const errorHandler = (error: ErrorException, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).send({ error: error.msg, error_code: error.status })
+  res.status(500).json({ error: error.msg, error_code: error.status })
 }
 
 module.exports = errorHandler
