9c69171fd8f9b7e3f6b1fc446f61e4a21149b166
Bobbyzzz <tianhui@ndmglobal.com>
2022-10-13 22:00:05 -1000

    fix: rename variables

--- functions/src/resetful-api/route/users.ts
+++ functions/src/resetful-api/route/users.ts
@@ -8,16 +8,16 @@ const UserService = require('../service/user_service')
 user_router.route('/').get(async (req: Request, res: Response) => {
   res.status(200).json({ 'data': await UserService.getUserList() })
 })
-user_router.route('/:id').get(async (req: Request, res: Response) => {
-  const user_id: string = req.params.id
-  res.status(200).json({ 'data': await UserService.getUserByUID(user_id) })
+user_router.route('/:uid').get(async (req: Request, res: Response) => {
+  const user_uid: string = req.params.uid
+  res.status(200).json({ 'data': await UserService.getUserByUID(user_uid) })
 })
 
 // /* PUT */
 // Update user
-user_router.route('/:id').put(async (req: Request, res: Response) => {
-  const user_id: string = req.params.id
-  res.status(201).json({ 'data': await UserService.updateUser(user_id, req.body) })
+user_router.route('/:uid').put(async (req: Request, res: Response) => {
+  const user_uid: string = req.params.uid
+  res.status(201).json({ 'data': await UserService.updateUser(user_uid, req.body) })
 })
 
 // /* DELETE */
